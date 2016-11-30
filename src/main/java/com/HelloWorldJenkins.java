cfd4c5c8747722c22b454a7d736c4bdd2a06de04
Badion <badionv@yahoo.com>
2016-11-30 18:02:48 +0200

    initial commit

---
+++ src/main/java/com/HelloWorldJenkins.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * Created by Vitaii Badion on 30.11.2016.
+ */
+public class HelloWorldJenkins {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world jenkins");
+    }
+}
